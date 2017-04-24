3ce0fad34b93f147727b7be1f417bc1341759de9
Erin Luu <eluu@kpd-i.com>
2017-04-24 12:38:39 -0400

    Initial Commit

---
+++ LearningAdvanceSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearningAdvanceSwift
+//
+//  Created by Erin Luu on 2017-04-24.
+//  Copyright © 2017 Erin Luu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
